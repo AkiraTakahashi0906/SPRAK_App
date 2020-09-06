702869f832f8293c55c69ee74c7c512e837a136c
akira takahashi <akira.takahashi.s2000@gmail.com>
2020-09-07 00:06:01 +0900

    0907

---
+++ SPRAK.Domain/Helpers/BarcodeImageCreate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Documents;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using ZXing;
+
+namespace SPRAK.Domain.Helpers
+{
+    public static class BarcodeImageCreate
+    {
+        public static void CreatePng(string barcodeText, string filePath)
+        {
+            //Nuget ZXing.Net
+            var bacodeWriter = new BarcodeWriter();
+
+            // バーコードの種類
+            bacodeWriter.Format = BarcodeFormat.CODE_128;
+
+            // サイズ
+            bacodeWriter.Options.Height = 100;
+            bacodeWriter.Options.Width = 300;
+
+            // バーコード左右の余白
+            bacodeWriter.Options.Margin = 30;
+
+            // バーコードのみ表示するか
+            // falseにするとテキストも表示する
+            bacodeWriter.Options.PureBarcode = false;
+
+            // バーコードBitmapを作成
+            using (var bitmap = bacodeWriter.Write(barcodeText))
+            {
+                // 画像として保存
+                bitmap.Save(filePath, ImageFormat.Png);
+            }
+
+            using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                // FileStreamからBitmapDecoderを作成します。
+                // BitmapCacheOptionをOnLoadにすることで画像データをメモリにキャッシュします。
+                var decoder = BitmapDecoder.Create(fs, BitmapCreateOptions.None, BitmapCacheOption.OnLoad);
+                var ImageSource = decoder.Frames[0];
+            }
+
+        }
+    }
+}
--- SPRAK_App/ViewModels/ShelfDataEditViewModel.cs
+++ SPRAK_App/ViewModels/ShelfDataEditViewModel.cs
@@ -1,11 +1,18 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using SPRAK.Domain.Entity;
+using SPRAK.Domain.Helpers;
 using SPRAK.Domain.Repositories;
 using SPRAK.Infrastructure;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Printing;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media.Imaging;
+using System.Windows.Xps;
 
 namespace SPRAK_App.ViewModels
 {
@@ -35,6 +42,50 @@ namespace SPRAK_App.ViewModels
         public DelegateCommand PrintButton { get; }
         public DelegateCommand UpdateButton { get; }
 
+        private void PrintBoxBarcode(string filePath)
+        {
+            //コンパイルするためにはWPFアプリケーションのプロジェクトに
+            //「System.Printing」と「ReachFramework」の参照設定を加える必要があります。
+            //WPFアプリケーションで印刷を行うにはこれらの参照設定が必要になります。
+            // 1.各種オブジェクトの生成
+            LocalPrintServer lps = new LocalPrintServer();
+            PrintQueue queue = lps.DefaultPrintQueue;
+            XpsDocumentWriter writer = PrintQueue.CreateXpsDocumentWriter(queue);
+
+            // 2. 用紙サイズの設定
+            PrintTicket ticket = queue.DefaultPrintTicket;
+            ticket.PageMediaSize = new PageMediaSize(PageMediaSizeName.ISOA4);
+            ticket.PageOrientation = PageOrientation.Portrait;
+
+            // 3. FixedPage の生成
+            FixedPage page = new FixedPage();
+
+            // 4. 印字データの作成
+            Canvas canvas = new Canvas();
+            TextBlock tb = new TextBlock();
+
+            System.Windows.Controls.Image image = new System.Windows.Controls.Image();
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(filePath, UriKind.Relative);
+            bitmapImage.EndInit();
+            image.Source = bitmapImage;
+
+            Canvas.SetTop(image, 200);
+            Canvas.SetLeft(image, 100);
+            canvas.Children.Add(image);
+
+            tb.Text = SelectedBoxData.BoxSetAssyNo;
+            tb.FontSize = 24;
+            Canvas.SetTop(tb, 100);
+            Canvas.SetLeft(tb, 100);
+            canvas.Children.Add(tb);
+            page.Children.Add(canvas);
+
+            // 5. 印刷の実行
+            writer.Write(page, ticket);
+        }
+
         private void UpdateButtonExecute()
         {
 
@@ -42,7 +93,9 @@ namespace SPRAK_App.ViewModels
 
         private void PrintButtonExecute()
         {
-
+            var filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "Barcode.png");
+            BarcodeImageCreate.CreatePng(SelectedBoxData.BoxBarcodeText, filePath);
+            PrintBoxBarcode(filePath);
         }
 
         private void BoxSelectedButtonExecute()
